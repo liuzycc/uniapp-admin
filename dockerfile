aa8a00999a7f8426263c320726f1188514be1bac
liuzy8 <liuzy8@ziroom.com>
2024-05-31 15:15:42 +0800

    feat: dockerfile

---
+++ dockerfile
@@ -0,0 +1,21 @@
+# 构建阶段
+FROM node:lts-alpine as build-stage
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+# 部署阶段
+FROM nginx:stable-alpine as production-stage
+
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,4 +1,5 @@
 export type EnvTypes = "development" | "test" | "staging" | "production";
 export const env = import.meta.env.VITE_ENV;
 export const isProd = env === "prod" || false;
-export const baseUrl = env === "prod" ? "xxx" : "http://localhost:3000";
+export const baseUrl =
+  env === "prod" ? "http://82.157.175.138:3001" : "http://localhost:3000";
