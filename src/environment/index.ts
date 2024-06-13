a44cf24c7f2f05a0ad6bf4e7983e3ca3a9cc1d8b
liuzy8 <liuzy8@ziroom.com>
2024-06-13 16:04:09 +0800

    feat: 切换域名接口

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -2,4 +2,4 @@ export type EnvTypes = "development" | "test" | "staging" | "production";
 export const env = import.meta.env.VITE_ENV;
 export const isProd = env === "prod" || false;
 export const baseUrl =
-  env === "prod" ? "http://82.157.175.138:3001" : "http://localhost:3000";
+  env === "prod" ? "https://service.longcheng.live" : "http://localhost:3000";
